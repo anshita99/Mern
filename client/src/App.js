bca8244af1611941c69314458071561dacae76d8
anshita99 <anshitagarg1999@gmail.com>
2021-03-28 20:05:50 +0530

    mern

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
       <Route exact path="/" component={Home}/>
       <UnPrivateRoute path="/login" component={Login}/>
       <UnPrivateRoute path="/register" component={Register}/>
-      <PrivateRoute path="/todos" roles={["user","admin"]} component={Todos}/>
+      <PrivateRoute path="/todos" roles={["admin"]} component={Todos}/>
       <PrivateRoute path="/admin" roles={["admin"]} component={Admin}/>
     </Router>
   );
